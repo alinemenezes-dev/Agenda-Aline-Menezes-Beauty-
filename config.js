bee415ce6ffa2f14b8836c5f3438e63cc3d58c55
alinemenezes-dev <alinemenezes.dev@gmail.com>
2025-10-16 23:01:32 -0300

    Add configuration for Google Maps link and message

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  googleMapsLink: 'https://www.google.com/maps/place/R.+Manoel+Ferraz+de+Campos+Sáles,+461+-+São+Cristóvão,+Chapecó+-+SC,+89804-092',
+  defaultMessage: `Olá [nome]! 🌸
+Passando pra lembrar do seu horário amanhã às [hora].
+Procedimento: [servico]💕
+📍 Como chegar: [link]
+Qualquer dúvida, pode me chamar por aqui!!!!`
+};
